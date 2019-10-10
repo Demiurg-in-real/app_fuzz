ac8c20183b364171d3174ff7f4f24997e61b09b1
Demiurg-in-real <bezginandrew@gmail.com>
2019-10-10 15:19:55 -0400

    auto

--- octopus_frenqi.cpp
+++ octopus_frenqi.cpp
@@ -7,16 +7,7 @@
 #include<cstring>
 #include<pthread.h>
 namespace fuzzing{
-	char iter[2];
 	static void *run(void *name1){
-		char *name=(char *)name1;
-		char *s = (char *)name[0];
-		pid_t parpid;
-		int wstatus;
-//		int counter=0;
-		char hs[2];
-		unsigned char y=0x0;
-//		std::cout <<"1\n";
 		for (){
 			parpid=vfork();
 			if(parpid == 0){
@@ -39,29 +30,6 @@ namespace fuzzing{
 	public:
 		threads();
 		~threads();
-	};
-	threads::threads(){
-		go();
-	}
-	threads::~threads(){
-		for (int i=0;i<2;i++){
-//			std::cout<<i<<"\n";
-			pthread_cancel(proc[i]);
-		}
-	}
-	void threads::go(){
-		unsigned char sen[3];
-		char *he="c.out";
-		(char*)(sen+0)=(char *)"c.out";
-		printf("%s\n",sen[0]);
-		//&sen[0]=(char*)"c.out";
-		for (int i=0;i<1;i++){
-			sen[1]=((char)i) * 128;
-			sen[2]=(char)i;
-			pthread_create(&proc[i],NULL,run,(void *)sen);
-			for(int l=0; l<3;l++) printf("%s --",sen[0]);
-//			pthread_join(proc[i], &res);
-		}
 	};
 }
 
